feb2223aef81011556a33fc59f7a0b19e595ab80
javier <javierms17@gmail.com>
2020-11-06 20:48:10 -0300

    login andando con apiclient

--- lib/base_app/api_client.dart
+++ lib/base_app/api_client.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
@@ -5,6 +6,7 @@ import 'package:elearning/data_types/course_dataType.dart';
 import 'package:elearning/data_types/couse_element_dataType.dart';
 import 'package:elearning/data_types/task_datatype.dart';
 import 'package:http/http.dart' as http;
+import 'package:elearning/base_app/user_credentials_data_type.dart';
 
 const String baseUrl = "http://localhost:8080";
 // var client = ApiClient();
@@ -18,14 +20,22 @@ class ApiClient {
 
   ApiClient._internal();
 
-  Future<String> login(String username, String password) async {    
-    var dio = Dio();
-    var response = await dio.post(baseUrl + '/login/',
-        data: {"username": username, "password": password});
-    print(response.data);
-    if (response.data != null) {
-      // login = true;
+  Future<bool> login({String username, String password}) async {
+    var response = await http.post(
+      '$baseUrl/login',
+      body: jsonEncode(<String, String>{
+        "username": username,
+        "password": password,
+      }),
+      headers: {"Content-Type": "application/json"},
+    );
+    if (response.statusCode == 200) {
+      storedUserCredentials.setToken(jsonDecode(response.body)["token"]);
+      storedUserCredentials.setNickname(username);
+      saveUserCredentials();
+      return true;
     }
+    return false;
   }
 
   Future<String> getUserInformation(String token) async {
--- lib/pages/tabbed_login_page.dart
+++ lib/pages/tabbed_login_page.dart
@@ -1,10 +1,16 @@
+import 'package:country_pickers/country.dart';
+import 'package:country_pickers/country_picker_dropdown.dart';
+import 'package:country_pickers/utils/utils.dart';
+import 'package:elearning/base_app/api_client.dart';
 import 'package:elearning/base_app/app_builder.dart';
 import 'package:elearning/base_app/user_credentials_data_type.dart';
 import 'package:elearning/tools/visual_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class TabbedLoginPage extends StatefulWidget {
-  TabbedLoginPage({Key key}) : super(key: key);
+  final int initialIndex;
+  TabbedLoginPage({Key key, this.initialIndex = 0}) : super(key: key);
 
   @override
   _TabbedLoginPageState createState() => _TabbedLoginPageState();
@@ -12,25 +18,80 @@ class TabbedLoginPage extends StatefulWidget {
 
 class _TabbedLoginPageState extends State<TabbedLoginPage>
     with TickerProviderStateMixin {
+  TabController _nestedTabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nestedTabController = new TabController(length: 2, vsync: this);
+    _nestedTabController.index = widget.initialIndex;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nestedTabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Material(
       child: Scaffold(
         body: Container(
           //* BACKGROUND
           decoration: tabbedLoginHeaderBoxDecoration,
-          child: Column(
-            children: [
-              Expanded(child: Container()),
-              Container(
-                constraints: BoxConstraints(maxWidth: 700),
-                color: myAppTheme['PrimaryBackgroundColor'],
-                height: screenHeight * 0.5,
-                child: LoginTab(),
-              ),
-            ],
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                //* TAB BAR
+                Container(
+                  constraints: BoxConstraints(maxWidth: 700),
+                  width: screenWidth,
+                  color: myAppTheme['PrimaryBackgroundColor'],
+                  child: TabBar(
+                    controller: _nestedTabController,
+                    indicatorColor: myAppTheme['AccentColor'],
+                    labelColor: myAppTheme['AccentTextColor'],
+                    unselectedLabelColor: myAppTheme['SecondaryTextColor'],
+                    isScrollable: true,
+                    tabs: <Widget>[
+                      Tab(
+                        text: "Login",
+                      ),
+                      Tab(
+                        text: "Recuperar Password",
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  constraints: BoxConstraints(maxWidth: 700),
+                  //* TAB VIEW
+                  // THIS CONTAINER IS WHERE THE TAB DISPLAYS ITS PAGES
+                  color: myAppTheme['PrimaryBackgroundColor'],
+                  height: screenHeight * 0.5,
+                  child: TabBarView(
+                    controller: _nestedTabController,
+                    children: <Widget>[
+                      Container(
+                        //* FIRST TAB PAGE
+                        // LOGIN
+                        child: LoginTab(),
+                      ),
+                      Container(
+                        //* third TAB PAGE
+                        // SIGNUP
+                        child: RecuperarPasswordTab(),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.end,
+            ),
           ),
         ),
       ),
@@ -45,6 +106,31 @@ class LoginTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool login = false;
+    final String urlAutenticar = "http://localhost:8080/login";
+
+    Future<bool> obtenerToken(String username, String password) async {
+      //var resupuesta = await http.post(urlAutenticar,
+      //  body: {"username": username, "password": password});
+      //print(resupuesta.body);
+      var dio = Dio();
+      Response response = await dio.post(urlAutenticar,
+          data: {"username": username, "password": password});
+      print(response.data);
+      if (response.data != null) {
+        login = true;
+        // storedUserCredentials.token = response.data.toString();
+        // print(storedUserCredentials.token);
+        saveUserCredentials();
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LandingPageLayoutBuilder(),
+          ),
+        );
+      }
+    }
+
     final usernameController = TextEditingController();
     final passwordContrller = TextEditingController();
 
@@ -67,12 +153,12 @@ class LoginTab extends StatelessWidget {
                 controller: usernameController,
                 style: tabbedLoginInputTextStyle,
                 decoration: InputDecoration(
-                  hintText: 'Username',
+                  hintText: 'e-Mail',
                   contentPadding: const EdgeInsets.all(15),
                 ),
                 validator: (value) {
                   if (value.isEmpty) {
-                    return 'Please enter your username';
+                    return 'Por favor ingrese su e-Mail';
                   }
                   return null;
                 },
@@ -102,7 +188,7 @@ class LoginTab extends StatelessWidget {
                         ),
                         validator: (value) {
                           if (value.isEmpty) {
-                            return 'Please enter your password';
+                            return 'Ingrese password';
                           }
                           return null;
                         },
@@ -125,18 +211,23 @@ class LoginTab extends StatelessWidget {
                     ),
                     padding: const EdgeInsets.all(15),
                     textColor: myAppTheme['PrimaryActionButtonColor'],
-                    onPressed: () {
+                    onPressed: () async {
                       // //TODO do separate function with checks
                       if (_loginFormKey.currentState.validate()) {
-                        storedUserCredentials
-                            .setNickname(usernameController.text);
-                        saveUserCredentials();
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LandingPageLayoutBuilder(),
-                          ),
+                        bool valid = false;
+                        var client = ApiClient();
+                        valid = await client.login(
+                          username: usernameController.text,
+                          password: passwordContrller.text,
                         );
+                        if (valid) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LandingPageLayoutBuilder(),
+                            ),
+                          );
+                        }
                       }
                     },
                   ),
@@ -144,19 +235,14 @@ class LoginTab extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 100,
+              height: 20,
             ),
-            //* RECOVER PASSWORD
+            //* LOGIN WITH GOOGLE BUTTON
             FlatButton(
               color: Colors.black,
-              onPressed: () {
-                if (_loginFormKey.currentState.validate()) {
-                  storedUserCredentials.setNickname(usernameController.text);
-                  saveUserCredentials();
-                }
-              },
+              onPressed: () {},
               child: Text(
-                "Recover password",
+                "Login With Google",
                 style: hilightTextStyle,
               ),
             ),
@@ -164,6 +250,121 @@ class LoginTab extends StatelessWidget {
               height: 20,
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class RecuperarPasswordTab extends StatelessWidget {
+  //const LoginTab({Key key}) : super(key: key);
+  static final _signupFormKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final usernameController = TextEditingController();
+/*
+    Future<bool> recuperarPassword(String mail) async {
+      String recuperarPass = "http://localhost:8080/usuarios/recuperarContra/";
+
+      //var resupuesta = await http.post(urlAutenticar,
+      //  body: {"username": username, "password": password});
+      //print(resupuesta.body);
+      recuperarPass += mail;
+      print("link recuperarpass" + recuperarPass);
+      var dio = Dio();
+      Response response = await dio.post(recuperarPass);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => TabbedLoginPage(),
+        ),
+      );
+    }
+*/
+    return Container(
+      child: Form(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  "Recuperar Password",
+                  style: titleTextStyle,
+                ),
+              ),
+              //* USERNAME TEXT FIELD
+              Padding(
+                padding: tabbedLoginPagging,
+                child: TextFormField(
+                  controller: usernameController,
+                  style: tabbedLoginInputTextStyle,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    hintText: 'E-Mail',
+                    contentPadding: const EdgeInsets.all(15),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'ingresa tu E-mail';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: tabbedLoginPagging,
+                child: Row(
+                  children: <Widget>[
+                    //* SIGN UP BUTTON
+                    FlatButton(
+                      child: Text(
+                        'Recuperar Password',
+                        style: tabbedLoginInputTextStyle,
+                      ),
+                      shape: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: myAppTheme['AccentColor'],
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding: const EdgeInsets.all(15),
+                      textColor: myAppTheme['PrimaryActionButtonColor'],
+                      onPressed: () {
+                        //TODO do separate function with checks
+                        if (_signupFormKey.currentState.validate()) {
+                          //   recuperarPassword(usernameController.text);
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              //* SIGNUP WITH GOOGLE BUTTON
+
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
         ),
       ),
     );
