29cd893d81d50bd257d230f9bbeea57454273fbe
santiago baro <sbaro96@gmai.com>
2020-11-19 18:25:38 -0300

    firebase implementation start

---
+++ lib/base_app/uploader.dart
@@ -0,0 +1,22 @@
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+
+// class Uploader extends StatefulWidget {
+//   final File file;
+//   Uploader({Key key, this.file}) : super(key: key);
+
+//   @override
+//   _UploaderState createState() => _UploaderState();
+// }
+
+// class _UploaderState extends State<Uploader> {
+//   final FirebaseStorage _storage = FirebaseStorage(storageBucket: );
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: child,
+//     );
+//   }
+// }
