39675435f9ea4ceee7d7992bc9c15fcdcffb7c99
Santiago Baro <sbaro96@gmai.com>
2020-12-10 02:26:14 -0300

    deleted course-user comentary

--- lib/elements/course_information_card.dart
+++ lib/elements/course_information_card.dart
@@ -78,49 +78,6 @@ class CourseInformationCard extends StatelessWidget {
                   ),
                 ],
               ),
-              Container(
-                margin: EdgeInsets.only(left: 15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                  ),
-                  color: Colors.white24,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 5,
-                    left: 8,
-                    right: 8,
-                    bottom: 2,
-                  ),
-                  child: Text(
-                    'Comentario',
-                    style: informationTextStyle,
-                  ),
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.white24,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10,
-                    right: 10,
-                    bottom: 10,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Autem dolor sint enim tempora dolorem reprehenderit reprehenderit. Et quos voluptate dignissimos. Quia beatae itaque nihil sed quis nisi rem sed eveniet. Pariatur aliquid reprehenderit. Illum molestias ut ipsum facilis aliquam consectetur aut dolorum.',
-                      maxLines: 8,
-                      style: informationTextStyle,
-                    ),
-                  ),
-                ),
-              ),
             ],
           ),
         ),
